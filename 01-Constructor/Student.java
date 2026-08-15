6add23a6485b642bb2e10a4e178ed46e634bf460
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-15 22:27:33 +0530

    Add Student class with constructor and display method
    Add Student class with constructor and display method

--- 01-Constructor/Student.java
+++ 01-Constructor/Student.java
@@ -1,1 +1,22 @@
 //
+class Student{
+    String name;
+    int age;
+    String course;
+     Student(String name, int age, String Course){
+        this.name=name;
+        this.age=age;
+        this.course=Course;
+        
+     }
+     static void display(Student s){
+        System.out.println("Student name is: "+s.name);
+        System.out.println("Age: "+s.age);
+        System.out.println("Course: "+ s.course);
+        
+        }
+    public static void main(String[] args) {
+        Student s1=new Student("ANKIT",22,"BCA DS & AI");
+        display(s1);
+    }
+}
