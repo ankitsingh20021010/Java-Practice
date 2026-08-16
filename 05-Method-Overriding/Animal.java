5fef32e2708310472ec9b22acee91d8a42f608c0
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-16 07:03:57 +0530

    Implement method overriding for Animal, Dog, and Cat

--- 05-Method-Overriding/Animal.java
+++ 05-Method-Overriding/Animal.java
@@ -1,1 +1,24 @@
 
+public class Animal {
+    void sound(){
+        System.out.println("Animal make sound");
+    }
+    class Dog extends Animal{
+    void sound(){
+        System.out.println("Dog bark");
+    }
+}
+class Cat extends Animal{
+    void sound(){
+        System.out.println("Cat meows");
+    }
+}
+public static void main(String[] args) {
+    Animal a=new Animal();
+    a.sound();
+    Animal tomy=a.new Dog();
+    tomy.sound();
+    Animal bella=a.new Cat();
+    bella.sound();
+}
+}
